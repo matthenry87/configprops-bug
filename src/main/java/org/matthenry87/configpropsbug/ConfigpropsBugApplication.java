3a2f470c73f1e7b62c3ef6f6f5fbd7815469bdc8
Matt Henry <Matt.Henry@fisglobal.com>
2022-11-07 19:17:08 -0700

    minimize code

--- src/main/java/org/matthenry87/configpropsbug/ConfigpropsBugApplication.java
+++ src/main/java/org/matthenry87/configpropsbug/ConfigpropsBugApplication.java
@@ -2,6 +2,9 @@ package org.matthenry87.configpropsbug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ConfigpropsBugApplication {
@@ -9,6 +12,31 @@ public class ConfigpropsBugApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ConfigpropsBugApplication.class, args);
+    }
+
+    @Configuration
+    @ConfigurationProperties(prefix = "foo")
+    static class ConfigPropertiesConfigBean {
+
+        private final Environment environment;
+
+        private String bar;
+
+        public ConfigPropertiesConfigBean(Environment environment) {
+
+            this.environment = environment;
+        }
+
+        public String getBar() {
+
+            return bar;
+        }
+
+        public void setBar(String bar) {
+
+            this.bar = bar;
+        }
+
     }
 
 }
