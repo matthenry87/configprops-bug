372ceb81223019205e38da4cb0cc33c423f1b6a4
Matt Henry <Matt.Henry@fisglobal.com>
2022-11-08 10:53:15 -0700

    Update to demonstrate adding @Autowired to constructor doesn't fix the issue.

--- src/main/java/org/matthenry87/configpropsbug/ConfigpropsBugApplication.java
+++ src/main/java/org/matthenry87/configpropsbug/ConfigpropsBugApplication.java
@@ -1,5 +1,6 @@
 package org.matthenry87.configpropsbug;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -22,6 +23,7 @@ public class ConfigpropsBugApplication {
 
         private String bar;
 
+        @Autowired
         public ConfigPropertiesConfigBean(Environment environment) {
 
             this.environment = environment;
